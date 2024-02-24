e78a7915435fea57cc3fed3f1b865eb458564e7f
TavoDGant <61161002+TavoDGant@users.noreply.github.com>
2024-02-24 15:23:34 -0600

    Add files via upload

---
+++ src/test/java/com/odegante/jpaexercises/JpaexercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odegante.jpaexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaexercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
